b448f05a710289084316a7d60f0b1eed16f9fdc1
Diya-Garg <divya.garg@niit.com>
2018-02-12 18:36:03 +0530

    Master Page Commit

--- onlineshopping/src/main/java/com/niit/onlineshopping/controllers/PageController.java
+++ onlineshopping/src/main/java/com/niit/onlineshopping/controllers/PageController.java
@@ -12,29 +12,28 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value="/")
+	@RequestMapping(value={"/","home"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring Web MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClicksHome",true);
 		return mv;
 	}
 	
-	
-	@RequestMapping(value="/test")
-	//public ModelAndView test(@RequestParam("greeting")String greeting){
-	public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting){
+	@RequestMapping(value={"about"})
+	public ModelAndView about(){
 		ModelAndView mv=new ModelAndView("page");
-		if(greeting==null){
-			greeting="Hello";
-		}
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
 	}
 	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView testWithPathVariable(@PathVariable("greeting")String greeting){
+	@RequestMapping(value={"contact"})
+	public ModelAndView contact(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
+	
 }
